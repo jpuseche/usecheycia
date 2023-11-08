4497d32c43434d45c6a6adb66099514fe1141a68
Juan Pablo Useche <jpuseche@uninorte.edu.co>
2023-11-07 22:54:20 -0500

    fix(home): typing on card title

--- src/sections/home/Highlights.js
+++ src/sections/home/Highlights.js
@@ -9,7 +9,7 @@ function Highlights() {
                     <img className="rounded-xl shadow-xl" src={publicDir + "/img/services/planta_clasificacion_agua.jpg"} alt="filtros multimedia acero inoxidable" ></img>
                 </a>
                 <a className="flex flex-col bg-[#c8eefd] hover:scale-95 transition w-full lg:w-1/3 rounded-xl shadow-xl p-10 cursor-pointer" href="/services/boilers">
-                    <h3 className="text-2xl lg:text-2xl text-[#27374D] font-semibold mb-5">Revisa támbien nuestros servicios pasa calderas</h3>
+                    <h3 className="text-2xl lg:text-2xl text-[#27374D] font-semibold mb-5">Revisa támbien nuestros servicios para calderas</h3>
                     <img className="rounded-xl shadow-xl" src={publicDir + "/img/services/caldera_acuotubular.jpg"} alt="filtros multimedia acero inoxidable" ></img>
                 </a>
             </div>
