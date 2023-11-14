5ab6ac9a04f7f486f88f873b062d877f6a2b6285
Juan Pablo Useche <jpuseche@uninorte.edu.co>
2023-11-13 23:25:44 -0500

    fix(Gallery): removed unused variable

--- src/sections/service/Gallery.js
+++ src/sections/service/Gallery.js
@@ -1,5 +1,4 @@
 function Gallery(props) {
-    const cols = props.gallery.length
     const gridClass = (props.gallery.length < 4) ? "grid lg:grid-cols-3 grid-cols-1 max-w-screen-xl lg:px-24 px-20 gap-3" : "grid lg:grid-cols-4 grid-cols-1 max-w-screen-xl lg:px-0 px-20 gap-3";
 
     return (
