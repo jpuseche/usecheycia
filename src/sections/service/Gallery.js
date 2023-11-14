9f8a8a32a1dbf3e81c2a3dcc9584af0755909436
Juan Pablo Useche <jpuseche@uninorte.edu.co>
2023-11-13 22:48:29 -0500

    chore(gallery): changed style to rounded

--- src/sections/service/Gallery.js
+++ src/sections/service/Gallery.js
@@ -1,12 +1,12 @@
 function Gallery(props) {
     const cols = props.gallery.length
-    const gridClass = (props.gallery.length < 4) ? "grid lg:grid-cols-" + cols + " grid-cols-1 max-w-screen-xl lg:px-24 px-20" : "grid lg:grid-cols-4 grid-cols-1 max-w-screen-xl lg:px-0 px-20";
+    const gridClass = (props.gallery.length < 4) ? "grid lg:grid-cols-3 grid-cols-1 max-w-screen-xl lg:px-24 px-20 gap-3" : "grid lg:grid-cols-4 grid-cols-1 max-w-screen-xl lg:px-0 px-20 gap-3";
 
     return (
         <section className="flex flex-col pb-28 items-center lg:px-5">
             <div className={gridClass}>
                 {props.gallery.map(image => (
-                    <img key={image.key} className="shadow-xl" src={image.source} alt={image.alt}></img>
+                    <img key={image.key} className="shadow-xl rounded-md" src={image.source} alt={image.alt}></img>
                 ))}
             </div>
         </section>
